38fca7ddb4d4021d8e269564fc84c92780e9c859
Adebayo Mayowa <mayowatope119@gmail.com>
2021-04-13 14:56:49 -0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -6,6 +6,6 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res =>res.json())
     .then(json =>{
         fs.writeFile('posts.json',JSON.stringify(json), () => {
-            console.log('File written to posts.txt');
+            console.log('File written to posts.json');
          }); 
 }).catch(err => console.error("Error occured : ", err));
