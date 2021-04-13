e920e77ce159f0f937b26bcf5ad98b94fdd485c9
codewarrior4 <mayowatope119@gmail.com>
2021-04-13 14:54:36 -0700

    Fetch to file assignment

---
+++ fetch.js
@@ -0,0 +1,11 @@
+const fs= require('fs')
+
+const fetch = require('node-fetch')
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res =>res.json())
+    .then(json =>{
+        fs.writeFile('posts.json',JSON.stringify(json), () => {
+            console.log('File written to posts.txt');
+         }); 
+}).catch(err => console.error("Error occured : ", err));
